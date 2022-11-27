d9b4a6a0442ecd341a57394b2543b190261f1869
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-11-28 01:30:06 +0400

    Change html to pug

--- components/result-card.vue
+++ components/result-card.vue
@@ -1,21 +1,19 @@
-<template>
-  <v-card>
-    <div class="d-flex flex-no-wrap justify-space-between">
-      <v-avatar
-        class="ma-3"
+<template lang="pug">
+  v-card
+   .d-flex.flex-no-wrap.justify-space-between
+      v-avatar.ma-3(
         size="125"
         tile
-      >
-        <v-img src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"></v-img>
-      </v-avatar>
-      <div>
-        <v-card-title
-          class="text-h5"
+      )
+        v-img(
+          src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
+        )
+      .div
+        v-card-title.text-h5(
           v-text="`Supermodel`"
-        ></v-card-title>
+        )
 
-        <v-card-subtitle v-text="`Foster the People`"></v-card-subtitle>
-      </div>
-    </div>
-  </v-card>
+        v-card-subtitle(
+          v-text="`Foster the People`"
+        )
  </template>
