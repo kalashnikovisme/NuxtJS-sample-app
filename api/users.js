f70de46ea2c76c7bec19dc0f678dd01103df4dee
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-11-27 23:24:13 +0400

    Getting users with axios

--- api/users.js
+++ api/users.js
@@ -1,7 +1,11 @@
 const { Router } = require('express')
 const router = Router()
+const axios = require('axios')
 
 router.use('/users', (req, res) => {
+  axios.get('https://gist.githubusercontent.com/allaud/093aa499998b7843bb10b44ea6ea02dc/raw/c400744999bf4b308f67807729a6635ced0c8644/users.json').then((response) => {
+    res.json(response.data)
+  })
 })
 
 module.exports = router
