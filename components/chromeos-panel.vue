aec6bde7e2aba30cfbcb6404ab05d4a8f05803e9
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-11-27 23:18:05 +0400

    Add tray

--- components/chromeos-panel.vue
+++ components/chromeos-panel.vue
@@ -1,11 +1,18 @@
 <template lang="pug">
 .chromeos-panel
-  ChromeIcon
-  GmailIcon
-  SearchIcon
-  DriveIcon
-  YoutubeIcon
-  AppsIcon
+  .apps
+    ChromeIcon
+    GmailIcon
+    SearchIcon
+    DriveIcon
+    YoutubeIcon
+    AppsIcon
+  .tray
+    span.time
+      = (new Date()).toLocaleString('en-US', { hour: 'numeric',  minute: 'numeric', hour12: true })
+    WifiIcon
+    BatteryIcon
+    AvatarIcon
 </template>
 
 <script>
@@ -15,6 +22,9 @@ import SearchIcon from '~/assets/search.svg?inline'
 import DriveIcon from '~/assets/drive.svg?inline'
 import YoutubeIcon from '~/assets/youtube.svg?inline'
 import AppsIcon from '~/assets/apps.svg?inline'
+import WifiIcon from '~/assets/wifi.svg?inline'
+import BatteryIcon from '~/assets/battery.svg?inline'
+import AvatarIcon from '~/assets/avatar.svg?inline'
 
 export default {
   components: {
@@ -24,6 +34,9 @@ export default {
     DriveIcon,
     YoutubeIcon,
     AppsIcon,
+    WifiIcon,
+    BatteryIcon,
+    AvatarIcon,
   },
 };
 </script>
@@ -37,10 +50,18 @@ export default {
       padding-top: 16px
       padding-bottom: 16px
       display: flex
-      align-items: center
+      justify-content: space-between
       padding-left: 18px
+      padding-right: 18px
       margin-top: 65px
 
       *:not(:first-child)
         margin-left: 32px
+
+      .apps, .tray
+        display: flex
+        align-items: center
+
+      span.time
+        color: white
 </style>
